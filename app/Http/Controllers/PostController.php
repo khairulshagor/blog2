fc3280e4995c3d83502e56e61b99d6216a914ab9
khairulshagor <khairulsagor567@.com>
2019-10-07 02:34:11 +0600

    here i added fakers and seeder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+  public function all_post ()
+  {
+      $post = Post::all();
+      return $post;
+  }
 }
